cf764bd567acf7ab97bd3b71d98690bb76935412
Zylles <156844892+Zylles@users.noreply.github.com>
2024-04-08 14:45:18 +0300

    Create lcd-screen.ino

---
+++ lcd-screen.ino
@@ -0,0 +1,26 @@
+// C++ code
+#include <LiquidCrystal.h>
+
+int seconds = 0;
+
+LiquidCrystal lcd_1(12, 11, 5, 4, 3, 2);
+
+void setup()
+{
+  lcd_1.begin(16, 2); // Set up the number of columns and rows on the LCD.
+
+  // Print a message to the LCD.
+  lcd_1.print("Zylles");
+}
+
+void loop()
+{
+  // set the cursor to column 0, line 1
+  // (note: line 1 is the second row, since counting
+  // begins with 0):
+  lcd_1.setCursor(0, 1);
+  // print the number of seconds since reset:
+  lcd_1.print(seconds);
+  delay(1000); // Wait for 1000 millisecond(s)
+  seconds += 1;
+}
